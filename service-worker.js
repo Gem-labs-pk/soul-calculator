d449dc3717f90072ac4486ace217ca6863fb2067
Muhammad Saqib Zahid <m.saqibzahid.iqbal@gmail.com>
2026-02-16 21:36:48 +0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,6 +3,7 @@ const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
+  './icon.svg',
   'https://cdn.tailwindcss.com',
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@700&family=Outfit:wght@300;400;600;800&family=Noto+Nastaliq+Urdu:wght@400;700&display=swap'
